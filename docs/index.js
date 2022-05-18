304b77d713a225033223b74bbcc94035151176be
Alicia <aburgum95@gmail.com>
2022-05-18 19:56:56 -0400

    Main

---
+++ docs/index.js
@@ -0,0 +1,11 @@
+$("#toggle").on("click", function(){
+    $("#dropDown").slideToggle();
+  });
+
+  $("#toggle2").on("click", function(){
+    $("#dropDown2").slideToggle();
+  });
+
+  $("#toggle3").on("click", function(){
+    $("#dropDown3").slideToggle();
+  });
